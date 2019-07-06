06ad5e655ae29cdee91b3ef48b62ccf509f9fb94
Lieunoir <lieunoir@rezel.net>
2019-07-06 18:05:04 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { PostWithComments } from './posts.js';
+import EventList from './eventList.js';
+import EventView from './eventView.js';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Menu extends React.Component {
+    render() {
+        return (
+            <div className="menu">
+                <UserCard username="Colin" />
+                <h3>Partyline Events</h3>
+                <ul>
+                    <Link to="/"><li>Test 1</li></Link>
+                    <Link to="/test"><li>Test 2</li></Link>
+                    <Link to="/test2"><li>Test 3</li></Link>
+                </ul>
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class UserCard extends React.Component {
+    render() {
+        return (
+            <div className="user-card">
+                {this.props.username}
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <Menu />
+                    <Route path="/" exact component={EventList} />
+                    <Route path="/event/:uuid" component={EventView} />
+                    <Route path="/test" exact component={EventList} />
+                    <Route path="/post/:uuid" exact component={PostWithComments} />
+                </Router>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
