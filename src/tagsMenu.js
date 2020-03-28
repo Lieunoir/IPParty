d1608880d7e7c3f13444905647d626e1937fdd86
Lieunoir <lieunoir@rezel.net>
2020-03-28 17:12:33 +0100

    Add basic tags functions

--- src/tagsMenu.js
+++ src/tagsMenu.js
@@ -1,20 +1,77 @@
 import React from 'react';
+import './tags.css';
 
-class TagWidget extends React.Component {
+class TagsSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tagName: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+
+    handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            this.handleClick();
+        }
+    }
+
+    handleChange(event) {
+        this.setState({tagName: event.target.value });
+    }
+
+    handleClick() {
+        this.props.addTag(this.state.tagName);
+        this.setState({tagName: '' });
+    }
+
     render() {
         return(
             <div>
-                {this.props.name}
+                <span >
+                    <input onKeyDown={this.handleKeyDown} value={this.state.tagName} onChange={this.handleChange} className="tags-search-text" type="text" name="tagName" placeholder="Search..." />
+                </span>
+                <span className="tags-search-button-span">
+                    <button onClick={this.handleClick} className="tags-search-button">Add</button>
+                </span>
             </div>
         )
     }
 }
 
+class TagWidget extends React.Component {
+    render() {
+        return(
+            <span className="tags-widget">
+                {this.props.name}
+            </span>
+        )
+    }
+}
+
 class TagsMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tagList: props.tagList
+        };
+        this.addTag = this.addTag.bind(this);
+    }
+
+    addTag(name) {
+        this.setState({
+            tagList: [...this.state.tagList, {"name": name}]
+        });
+    }
+
     render() {
         return(
             <div>
-                {this.props.tagList.map(tag => (
+                <TagsSelector addTag={this.addTag}/>
+                {this.state.tagList.map(tag => (
                     <TagWidget name={tag.name} />
                 ))}
             </div>
