191fb2c9a81f7b057b6c8c8206634516bba55d98
ulysse <ulysse.jobard@telecom-paris.fr>
2020-04-09 16:14:38 +0200

    Interface Login

--- src/login.js
+++ src/login.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import './login.css'
 
 class Login extends React.Component {
     render() {
         return (
-            <div className="login">
-                <form onSubmit={this.props.login}>
-                    <input type="text" name="username"></input>
-                    <input type="password" name="password"></input>
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <button onClick={this.props.login}>Ha</button>
-            </div>
+			<div className="loginContainer">
+				<div id='IPParty'>
+					Welcom to IPParty
+				</div>
+	            <div className="login">
+	                <form onSubmit={this.props.login}>
+						<div className='line'>
+							Login :
+	                    	<input type="text" name="username" className='input'></input>
+						</div>
+						<div className='line'>
+							Password :
+	                    	<input type="password" name="password" className='input'></input>
+						</div>
+						<div id='submit'>
+	                    	<input type="submit" value="Submit"></input>
+						</div>
+	                </form>
+					<div className = 'create'>
+						<div id="noaccount">
+						Don't have an account yet?
+						</div>
+		                <button onClick={this.props.login} id='register'>Create Account</button>
+					</div>
+	            </div>
+			</div>
         );
     }
 }
